e01f6676e0b544e956a72819cad634a7f6ad5b62
xuyang141 <448762301@qq.com>
2026-05-30 10:55:46 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",function(e){self.skipWaiting();});self.addEventListener("activate",function(e){e.waitUntil(clients.claim());});self.addEventListener("fetch",function(e){e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
